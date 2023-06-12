3b8ddd464c416ae68ef47c4964e0cd7ed32d9dcc
Developsom <100343391+Developsom@users.noreply.github.com>
2023-06-12 13:09:58 +0200

    Moved call-button html into 'Call' component and uploaded my CV into new src/assets map

--- src/App.js
+++ src/App.js
@@ -5,7 +5,6 @@ function App() {
     return (
         <div className="App">
             <Header />
-            <a href="tel:+31613630011"><button>Call +31613630011</button></a>
         </div>
     );
 }
---
+++ src/components/Call.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Call() {
+    return (
+        <div className="Call">
+            <a href="tel:+31613630011"><button>Call +31613630011</button></a>
+        </div>
+    );
+}
+
+export default Call;
